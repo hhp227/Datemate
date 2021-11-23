06dde7570abccdc6da4912a976cc9a544f11f8a7
hhp227 <hhp227@kortek.co.kr>
2021-11-23 17:51:23 +0900

    2021.11.23 Commit

--- app/src/main/java/com/hhp227/datemate/MainActivity.kt
+++ app/src/main/java/com/hhp227/datemate/MainActivity.kt
@@ -1,75 +1,43 @@
-package com.hhp227.datemate;
+package com.hhp227.datemate
 
-import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuItem;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.auth.FirebaseAuth;
-import com.hhp227.datemate.fragment.MyPostsFragment;
-import com.hhp227.datemate.fragment.MyTopPostsFragment;
-import com.hhp227.datemate.fragment.RecentPostsFragment;
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kortek.myapplication.ui.theme.DateMateTheme
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ViewPager mViewPager = findViewById(R.id.container);
-        FragmentPagerAdapter mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-            private final Fragment[] mFragments = new Fragment[] {
-                    new RecentPostsFragment(),
-                    new MyPostsFragment(),
-                    new MyTopPostsFragment()
-            };
-            private final String[] mFragmentNames = new String[] {getString(R.string.heading_recent), getString(R.string.heading_my_posts), getString(R.string.heading_my_top_posts)};
-
-            @Override
-            public Fragment getItem(int position) {
-                return mFragments[position];
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DateMateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("DateMate")
+                }
             }
-
-            @Override
-            public int getCount() {
-                return mFragments.length;
-            }
-
-            @Override
-            public CharSequence getPageTitle(int position) {
-                return mFragmentNames[position];
-            }
-        };
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        FloatingActionButton floatingActionButton = findViewById(R.id.fab_new_post);
-
-        mViewPager.setAdapter(mPagerAdapter);
-        tabLayout.setupWithViewPager(mViewPager);
-        floatingActionButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, NewPostActivity.class)));
+        }
     }
+}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int i = item.getItemId();
-        if (i == R.id.action_logout) {
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(this, SignInActivity.class));
-            finish();
-            return true;
-        } else
-            return super.onOptionsItemSelected(item);
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DateMateTheme {
+        Greeting("Android")
     }
 }
