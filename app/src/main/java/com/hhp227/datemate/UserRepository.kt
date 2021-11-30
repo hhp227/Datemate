02242a6b94c38e9266daaf09a398f80c8ca91e85
hhp227 <hhp227@kortek.co.kr>
2021-11-30 19:19:06 +0900

    2021.11.30 Commit

---
+++ app/src/main/java/com/hhp227/datemate/UserRepository.kt
@@ -0,0 +1,12 @@
+package com.hhp227.datemate
+
+class UserRepository {
+    fun signIn(email: String, password: String) {
+
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun signUp(email: String, password: String) {
+
+    }
+}
