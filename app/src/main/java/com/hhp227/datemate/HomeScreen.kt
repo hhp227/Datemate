990b3786e8b821580eb9a84fbeaf98b1fe971c70
hhp227 <hhp227@kortek.co.kr>
2021-12-07 17:33:47 +0900

    2021.12.07 Commit

--- app/src/main/java/com/hhp227/datemate/HomeScreen.kt
+++ app/src/main/java/com/hhp227/datemate/HomeScreen.kt
@@ -1,9 +1,26 @@
 package com.hhp227.datemate
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.kortek.myapplication.ui.theme.DateMateTheme
 
 @Composable
 fun HomeScreen() {
     Text("Home")
+}
+
+@Preview(name = "Home in light theme")
+@Composable
+fun HomePreview() {
+    DateMateTheme {
+        HomeScreen()
+    }
 }
